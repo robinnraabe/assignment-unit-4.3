120d64f95bb8e6d1ff2b06deca6158c5c42b072a
Robin Raabe <robinnraabe@gmail.com>
2023-08-30 13:58:17 -0500

    group work done

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,16 +2,61 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = [];
+const maxItems = 5;
+let fullBool = false;
 
-function addItem() {
-    basket.push('Kale');
+function isFull() {
+    if (basket.length < maxItems) {
+        fullBool = false;
+    }
+    else {
+        fullBool = true;
+    }
+    return fullBool;
 }
 
+function addItem(item) {
+    if (isFull() == false) {
+        basket.push(item);
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
+function listItems(array) {
+    console.log('Basket contains:');
+    for (let item of array) {
+        console.log(item);
+    }
+}
 
+function empty(array) {
+    while (array.length > 0) {
+        array.pop();
+    }
+    return array;
+}
 
+function removeItem(item) {
+    let index = basket.indexOf(item);
+    if (index == -1) {
+        return null;
+    }
+    else {
+        removedItem = basket.splice(index, 1);
+        return removedItem.toString();
+    }
+}
 
-
+addItem('Celery');
+addItem('Carrot');
+addItem('Apple');
+listItems(basket);
+console.log('*** Removed', removeItem('Apple'));
+console.log(`Basket is now ${basket}`);
+console.log('Emptied basket', empty(basket))
 
 
 
